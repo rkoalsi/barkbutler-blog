909321a63d136520133da3a26b25d2e5c33b3866
rkoalsi <57904969+rkoalsi@users.noreply.github.com>
2023-02-09 11:46:42 +0530

    Adding in google tag for seo

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import TwSizeIndicator from "@components/TwSizeIndicator";
 import config from "@config/config.json";
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 const Document = () => {
   // destructuring items from config object
@@ -10,6 +11,18 @@ const Document = () => {
       <Head>
         {/* favicon */}
         <link rel="shortcut icon" href={favicon} />
+        <Script
+          id={"googleTagManager"}
+          strategy="afterInteractive"
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-YBKE0R685E"
+        />
+        <Script strategy="afterInteractive" id={"googleConfig"}>
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-YBKE0R685E');`}
+        </Script>
         {/* color meta */}
         <meta name="msapplication-TileColor" content="#000000" />
         <meta
